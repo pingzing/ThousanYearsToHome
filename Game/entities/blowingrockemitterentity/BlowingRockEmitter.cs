0dedd15fe243f09cb6e595bd7abc251356a14de5
pingzing <mcalistern@gmail.com>
2023-09-23 23:01:59 +0300

    Add rough draft of  blowing rock enities

---
+++ Game/entities/blowingrockemitterentity/BlowingRockEmitter.cs
@@ -0,0 +1,72 @@
+using Godot;
+
+namespace ThousandYearsHome.Entities.BlowingRockEmitterEntity
+{
+
+    [Tool]
+    public class BlowingRockEmitter : Node2D
+    {
+        // In seconds.
+        private float _rockSpawnTime = 5f;
+        [Export] public float RockSpawnTime
+        {
+            get => _rockSpawnTime;
+            set { _rockSpawnTime = value; Update(); }
+        }
+
+        // In degrees.
+        private float _spawnAngle = 0f;
+        [Export(PropertyHint.Range, "0, 359, 1")]
+        public float SpawnAngle
+        {
+            get => _spawnAngle;
+            set { _spawnAngle = value; Update(); }
+        }
+
+        private Timer _spawnTimer = null!;
+        private DynamicFont _debugFont = null!;
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            _spawnTimer = GetNode<Timer>("SpawnTimer");
+            _spawnTimer.WaitTime = RockSpawnTime;
+        }
+
+        public override void _Draw()
+        {
+            if (!Engine.EditorHint)
+            {
+                return;
+            }
+
+            if (_debugFont == null)
+            {
+                _debugFont = (DynamicFont)GD.Load("res://fonts/Equipment-base.tres");
+            }
+
+            float arrowLength = 10f;
+            float angleRadians = Mathf.Deg2Rad(SpawnAngle);
+            float targetX = arrowLength * Mathf.Cos(angleRadians);
+            float targetY = arrowLength * Mathf.Sin(angleRadians);
+            Vector2 target = new Vector2(targetX, targetY);
+
+            float armLength = 7f;
+            float armAngle1Radians = Mathf.Deg2Rad(SpawnAngle + 180 + 35);
+            float arm1TargetX = target.x + (armLength * Mathf.Cos(armAngle1Radians));
+            float arm1TargetY = target.y + (armLength * Mathf.Sin(armAngle1Radians));
+            Vector2 arm1Target = new Vector2(arm1TargetX, arm1TargetY);
+
+            float armAngle2Radians = Mathf.Deg2Rad(SpawnAngle + 180 - 35);
+            float arm2TargetX = target.x + (armLength * Mathf.Cos(armAngle2Radians));
+            float arm2TargetY = target.y + (armLength * Mathf.Sin(armAngle2Radians));
+            Vector2 arm2Target = new Vector2(arm2TargetX, arm2TargetY);
+            
+            DrawLine(Vector2.Zero, target, Colors.Green, width: 2);
+            DrawLine(target, arm1Target, Colors.Green, width: 2);
+            DrawLine(target, arm2Target, Colors.Green, width: 2);
+
+            DrawString(_debugFont, new Vector2(0, 10), $"{RockSpawnTime}s");
+        }
+    }
+}
